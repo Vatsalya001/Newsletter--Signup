2c84ed4ecef88837e1f2bb42ee8bed6ca4e6f451
Vatsalya Roy <79377087+Vatsalya001@users.noreply.github.com>
2022-07-11 20:00:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+//jshint esversion: 6
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);      //to be send to mailchimp via api
+
+  const url = "https://us9.api.mailchimp.com/3.0/lists/7c95b24628";
+  const options = {
+    method: "POST",
+    auth: "vatsalya1:806e916d4987c456cdb69bd4bb957814-us9"
+  };
+
+
+  const request = https.request(url, options, function(response){
+    console.log(response.statusCode);
+
+    if(response.statusCode === 200){
+      res.sendFile(__dirname+"/success.html");
+    }
+    else{
+      res.sendFile(__dirname+"/failure.html");
+    }
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+app.get("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000");
+});
+// 806e916d4987c456cdb69bd4bb957814-us9
+//7c95b24628
